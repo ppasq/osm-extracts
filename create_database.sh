8e73f4d95c156e00d00c9d5e2327520016c50caa
Paolo Pasquali <paolo.pasquali@gmail.com>
2022-04-11 15:06:47 +0200

    Create create_database.sh

---
+++ create_database.sh
@@ -0,0 +1,8 @@
+## Create DB
+sudo -u postgres createdb -O geonode osm
+sudo -u postgres psql -d osm -c 'CREATE EXTENSION postgis;'
+sudo -u postgres psql -d osm -c 'CREATE EXTENSION hstore;'
+sudo -u postgres psql -d osm -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+sudo -u postgres psql -d osm -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+sudo -u postgres psql -d osm -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO geonode;'
+sudo -u postgres psql -d osm -c 'GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO geonode;'
