9474c22502fd1063f80fcb1851ea84ef78bbdea0
Paolo Pasquali <paolo.pasquali@gmail.com>
2022-04-11 15:09:38 +0200

    Create set_table_owner.sql

---
+++ set_table_owner.sql
@@ -0,0 +1,4 @@
+ALTER TABLE planet_osm_line OWNER TO geonode;
+ALTER TABLE planet_osm_point OWNER TO geonode;
+ALTER TABLE planet_osm_polygon OWNER TO geonode;
+ALTER TABLE planet_osm_roads OWNER TO geonode;
