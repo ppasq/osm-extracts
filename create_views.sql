4077d7eaee2db91b0a2d52bbe7cf9a826345b9a2
ppasq <paolo.pasquali@gmail.com>
2022-04-11 16:15:39 +0200

    Fix view ownership

--- create_views.sql
+++ create_views.sql
@@ -3,13 +3,13 @@ CREATE SCHEMA geonode AUTHORIZATION geonode;
 
 # Aerialway
 CREATE VIEW geonode.piemonte_aerialway_line AS SELECT osm_id, name, aerialway, ref, way FROM planet_osm_line WHERE aerialway in ('cable_car', 'gondola', 'mixed_lift', 'chair_lift', 'drag_lift', 't-bar', 'j-bar', 'platter', 'rope_tow', 'magic_carpet', 'zip_line', 'goods', 'pylon', 'station');
-ALTER VIEW geonode.piemonte_aerialway_line OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_aerialway_line OWNER TO geonode;
 
 # Aeroway
 CREATE VIEW geonode.piemonte_aeroway_polygon AS SELECT osm_id, name, aeroway, ref, way FROM planet_osm_polygon WHERE aeroway in ('aerodrome', 'apron', 'hangar', 'helipad', 'heliport', 'spaceport', 'terminal');
 CREATE VIEW geonode.piemonte_aeroway_point AS SELECT osm_id, name, aeroway, ref, way FROM planet_osm_point WHERE aeroway in ('aerodrome', 'gate', 'helipad', 'heliport', 'navigationaid', 'spaceport', 'terminal', 'windsock');
-ALTER VIEW geonode.piemonte_aeroway_polygon OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_aeroway_point OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_aeroway_polygon OWNER TO geonode;
+ALTER VIEW geonode.piemonte_aeroway_point OWNER TO geonode;
 
 #Amenity
 CREATE VIEW geonode.piemonte_amenity_sustenance_point AS SELECT osm_id, name, amenity, ref, way FROM planet_osm_point WHERE amenity in ('bar', 'biergarten', 'cafe', 'fast_food', 'food_court', 'ice_cream', 'pub', 'restaurant');
@@ -22,38 +22,38 @@ CREATE VIEW geonode.piemonte_amenity_public_service_point AS SELECT osm_id, name
 CREATE VIEW geonode.piemonte_amenity_facilities_point AS SELECT osm_id, name, amenity, ref, way FROM planet_osm_point WHERE amenity in ('bbq','bench','dog_toilet','drinking_water','give_box','parcel_locker','shelter','shower','telephone','toilets','water_point','watering_place');
 CREATE VIEW geonode.piemonte_amenity_waste_management_point AS SELECT osm_id, name, amenity, ref, way FROM planet_osm_point WHERE amenity in ('sanitary_dump_station','recycling','waste_basket','waste_disposal','waste_transfer_station');
 CREATE VIEW geonode.piemonte_amenity_others_point AS SELECT osm_id, name, amenity, ref, way FROM planet_osm_point WHERE amenity in ('animal_boarding','animal_breeding','animal_shelter','baking_oven','childcare','clock','crematorium','dive_centre','funeral_hall','grave_yard','hunting_stand','internet_cafe','kitchen','kneipp_water_cure','lounger','marketplace','monastery','photo_booth','place_of_mourning','place_of_worship','public_bath','refugee_site','vending_machine');
-ALTER VIEW geonode.piemonte_amenity_sustenance_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_education_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_transportation_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_financial_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_healthcare_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_entertainment_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_public_service_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_facilities_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_waste_management_point OWNER TO ithaca;
-ALTER VIEW geonode.piemonte_amenity_others_point OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_amenity_sustenance_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_education_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_transportation_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_financial_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_healthcare_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_entertainment_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_public_service_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_facilities_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_waste_management_point OWNER TO geonode;
+ALTER VIEW geonode.piemonte_amenity_others_point OWNER TO geonode;
 
 #Barriers
 CREATE VIEW geonode.piemonte_barrier_line AS SELECT osm_id, name, barrier, ref, way FROM planet_osm_line WHERE barrier in ('cable_barrier', 'city_wall', 'ditch', 'fence', 'guard_rail', 'hand_rail', 'hedge', 'kerb', 'retaining_wall', 'wall');
-ALTER VIEW geonode.piemonte_barrier_line OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_barrier_line OWNER TO geonode;
 
 #Boundary
 CREATE VIEW geonode.piemonte_administrative_boundary_polygon AS SELECT osm_id, name, boundary, admin_level, way FROM planet_osm_polygon WHERE boundary = 'administrative';
-ALTER VIEW geonode.piemonte_administrative_boundary_polygon OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_administrative_boundary_polygon OWNER TO geonode;
 CREATE VIEW geonode.piemonte_park_boundary_polygon AS SELECT osm_id, name, boundary, way FROM planet_osm_polygon WHERE boundary in ('national_park', 'protected_area');
-ALTER VIEW geonode.piemonte_park_boundary_polygon OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_park_boundary_polygon OWNER TO geonode;
 
 #Building
 CREATE VIEW geonode.piemonte_building_polygon AS SELECT osm_id, name, building, "addr:housename", "addr:housenumber", "addr:interpolation", way FROM planet_osm_polygon WHERE building IS NOT NULL;
-ALTER VIEW geonode.piemonte_building_polygon OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_building_polygon OWNER TO geonode;
 
 #Craft
 CREATE VIEW geonode.piemonte_craft_polygon AS SELECT osm_id, name, craft, way FROM planet_osm_polygon WHERE craft IS NOT NULL;
-ALTER VIEW geonode.piemonte_craft_polygon OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_craft_polygon OWNER TO geonode;
 
 #Emergency
 CREATE VIEW geonode.piemonte_emergency_point AS SELECT osm_id, name, emergency, way FROM planet_osm_point WHERE emergency IS NOT NULL;
-ALTER VIEW geonode.piemonte_emergency_point OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_emergency_point OWNER TO geonode;
 
 #Geological
 
@@ -61,7 +61,7 @@ ALTER VIEW geonode.piemonte_emergency_point OWNER TO ithaca;
 
 #Highway
 CREATE VIEW geonode.piemonte_highway_line AS SELECT osm_id, name, ref, bridge, tunnel, way FROM planet_osm_roads WHERE highway IS NOT NULL;
-ALTER VIEW geonode.piemonte_highway_line OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_highway_line OWNER TO geonode;
 
 #Historic
 
@@ -83,9 +83,9 @@ ALTER VIEW geonode.piemonte_highway_line OWNER TO ithaca;
 
 #Railway
 CREATE VIEW geonode.piemonte_railway_line AS SELECT osm_id, name, railway, bridge, cutting, embarkment, service, tunnel, way FROM planet_osm_line WHERE railway in ('abandoned', 'construction','disused','funicular','light_rail','miniature','monorail','narrow_gauge','preserved','rail','subway','tram');
-ALTER VIEW geonode.piemonte_railway_line OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_railway_line OWNER TO geonode;
 CREATE VIEW geonode.piemonte_railway_point AS SELECT osm_id, name, railway, way FROM planet_osm_point WHERE railway in ('halt', 'stop_position', 'station', 'subway_entrance', 'tram_stop');
-ALTER VIEW geonode.piemonte_railway_point OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_railway_point OWNER TO geonode;
 #Route
 
 #Shop
@@ -98,8 +98,8 @@ ALTER VIEW geonode.piemonte_railway_point OWNER TO ithaca;
 
 #Water
 CREATE VIEW geonode.piemonte_water_polygon AS SELECT osm_id, name, "natural", water, way FROM planet_osm_polygon WHERE water in ('river', 'oxbow', 'canal', 'ditch', 'lock', 'fish_pass', 'lake', 'reservoir', 'pond', 'basin', 'lagoon', 'stream_pool', 'reflecting_pool', 'moat', 'watewater');
-ALTER VIEW geonode.piemonte_water_polygon OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_water_polygon OWNER TO geonode;
 
 #Waterway
 CREATE VIEW geonode.piemonte_waterway_line AS SELECT osm_id, name, waterway, way FROM planet_osm_line WHERE waterway in ('river', 'riverbank', 'stream', 'tidal_channel', 'canal', 'drain', 'ditch', 'pressurised', 'fairway', 'dam', 'weir', 'waterfall', 'lock_gate');
-ALTER VIEW geonode.piemonte_waterway_line OWNER TO ithaca;
+ALTER VIEW geonode.piemonte_waterway_line OWNER TO geonode;
